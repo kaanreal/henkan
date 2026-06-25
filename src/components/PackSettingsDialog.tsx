1d3d20bb61fb3338cfb0c5ae4f4d81b6eea7e9bb
Kaan <kaan.cat@icloud.com>
2026-06-25 22:16:07 +0200

    Revert "fix: Fixed bug where you couldnt type in the pack creator field"
    This reverts commit 5399cafc9dcf90c53408e5a0919d4d0e79d8187d.

--- src/components/PackSettingsDialog.tsx
+++ src/components/PackSettingsDialog.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 interface PackSettings {
   mode: 'osz' | 'folder'
@@ -21,13 +21,12 @@ export function PackSettingsDialog({ open, packName, defaultSettings, isConverti
   const [leaving, setLeaving] = useState(false)
 
   // Reset state when dialog opens
-  useEffect(() => {
-    if (open) {
-      setSettings(defaultSettings)
-      setLeaving(false)
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [open])
+  if (open && leaving) {
+    setLeaving(false)
+  }
+  if (open && (settings.mode !== defaultSettings.mode || settings.creator !== defaultSettings.creator)) {
+    setSettings(defaultSettings)
+  }
 
   function update<K extends keyof PackSettings>(key: K, value: PackSettings[K]) {
     setSettings(prev => ({ ...prev, [key]: value }))
